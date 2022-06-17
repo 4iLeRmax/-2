69538be257ff8a2e5790822a6103c8cc426bb6a5
4iLeRmax <102809824+4iLeRmax@users.noreply.github.com>
2022-06-17 16:16:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let nav = document.querySelector('nav');
+let links = document.querySelector('.navLinks');
+// nav.style.background = "#fff";
+function menu(){
+  nav.classList.toggle('white');
+  
+}
